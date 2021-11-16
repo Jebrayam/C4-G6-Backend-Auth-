474784571da5def0e7ee3e77f2add30807f74f00
Gabriel Morales <gabrielmorales1@pm.me>
2021-11-15 21:49:33 -0500

    Configuración básica de JWT

--- c4_mercar/settings_dev.py
+++ c4_mercar/settings_dev.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,18 @@ THIRD_APPS = [
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME"   : timedelta(minutes = 15),
+    "REFRESH_TOKEN_LIFETIME"  : timedelta(days = 1),
+    "ROTATE_REFRESH_TOKENS"   : False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN"       : False,
+
+    "ALGORITHM"               : "HS256",
+    "USER_ID_FIELD"           : "id",
+    "USER_ID_CLAIM"           : "user_id"
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -58,6 +71,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 ROOT_URLCONF = 'c4_mercar.urls'
 
