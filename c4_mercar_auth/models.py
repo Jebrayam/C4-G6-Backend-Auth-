eb00aeb5efe169e98e7a0c654de6d5969bf83186
Gabriel Morales <gabrielmorales1@pm.me>
2021-11-16 20:20:29 -0500

    Fix: Las peticiones al API fallaban
    Al estar el password no encriptado las peticiones al API REST daban
    problemas de autenticación, se solucionó hasheando el password.

--- c4_mercar_auth/models.py
+++ c4_mercar_auth/models.py
@@ -1,9 +1,9 @@
+# Django
 from django.db import models
-
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
-
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models  import AbstractBaseUser
+from django.contrib.auth.models  import PermissionsMixin
+from django.contrib.auth.models  import BaseUserManager
+from django.contrib.auth.hashers import make_password
 
 class UserProfileManager(BaseUserManager):
     def create_user(self, email, first_name, last_name, phone_number, address,  password = None):
@@ -67,5 +67,10 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     )
 
     objects = UserProfileManager()
+
+    def save(self, **kwargs):
+        some_salt = 'mMUj0DrIK6vgtdIYepkIxN'
+        self.password = make_password(self.password, some_salt)
+        super().save(**kwargs)
 
     USERNAME_FIELD = "email"
